842956a3502ac99c83f85b3f3f23efcde2fa0322
Abigya Ayele <abigya.ayele.12@gmail.com>
2022-07-11 23:22:35 +0300

    edit que8.hs

--- Book/5. Haskell_basics/exercises/que8.hs
+++ Book/5. Haskell_basics/exercises/que8.hs
@@ -10,11 +10,17 @@ merge [] (y:y1:ys) | (y < y1) = [y] ++ merge [] (y1:ys)
 merge (x:[]) (y:y1:ys) | ((x < y) && (y < y1)) = [x,y] ++ merge [] (y1:ys)
                        | ((x > y) && (y < y1)) = [y] ++ merge (x:[]) (y1:ys)
                        | ((x == y) && (y < y1)) = [x] ++ merge [] (y1:ys)
+                       | ((x < y) && (y == y1)) = [x,y] ++ merge [] (ys)
+                       | ((x > y) && (y == y1)) = [y] ++ merge (x:[]) (ys)
+                       | ((x == y) && (y == y1)) = [x] ++ merge [] (ys)
                        | otherwise = error ("Lists must be increasing")
 
 merge (x:x1:xs) (y:[]) | ((x < y) && (x < x1)) = [x] ++ merge (x1:xs) (y:[])
                        | ((x > y) && (x < x1)) = [y,x] ++ merge (x1:xs) []
                        | ((x == y) && (x < x1)) = [x] ++ merge (x1:xs) []
+                       | ((x < y) && (x == x1)) = [x] ++ merge (xs) (y:[])
+                       | ((x > y) && (x == x1)) = [y,x] ++ merge (xs) []
+                       | ((x == y) && (x == x1)) = [x] ++ merge (xs) []
                        | otherwise = error ("Lists must be increasing")
 
 merge (x:[]) (y:[]) | (x < y) = [x,y] 
@@ -24,17 +30,14 @@ merge (x:[]) (y:[]) | (x < y) = [x,y]
 merge [] (y:[]) = [y]
 merge (x:[]) [] = [x]
 
-merge (x:x1:xs) (y:y1:ys) | ((x < x1) && (y < x) && (y < y1)) = [y] ++ merge (x:x1:xs) (y1:ys)
+merge (x:x1:xs) (y:y1:ys) | ((x < x1) && (y < x) && (y <= y1)) = [y] ++ merge (x:x1:xs) (y1:ys)
                           | ((x < x1) && (y > x) && (y < y1)) = [x] ++ merge (x1:xs) (y:y1:ys)
-                          | ((x < x1) && (y == x) && (y < y1)) = [x] ++ merge (x1:xs) (y1:ys)
-                          | ((x < x1) && (y < x) && (y == y1)) = [y] ++ merge (x:x1:xs) (y1:ys)
-                          | ((x < x1) && (y > x) && (y == y1)) = [x] ++ merge (x1:xs) (y1:ys)
+                          | ((x < x1) && (y >= x) && (y <= y1)) = [x] ++ merge (x1:xs) (y1:ys)
                           | ((x < x1) && (y == x) && (y == y1)) = [x] ++ merge (x1:xs) (ys)
                           | ((x == x1) && (y < x) && (y < y1)) = [y] ++ merge (x1:xs) (y1:ys)
                           | ((x == x1) && (y > x) && (y < y1)) = [x] ++ merge (xs) (y:y1:ys)
-                          | ((x == x1) && (y == x) && (y < y1)) = [x] ++ merge (xs) (y1:ys)
+                          | ((x == x1) && (y >= x) && (y <= y1)) = [x] ++ merge (xs) (y1:ys)
                           | ((x == x1) && (y < x) && (y == y1)) = [y] ++ merge (x1:xs) (ys)
-                          | ((x == x1) && (y > x) && (y == y1)) = [x] ++ merge (xs) (y1:ys)
                           | ((x == x1) && (y == x) && (y == y1)) = [x] ++ merge (xs) (ys)
                           | otherwise = error ("Lists must be increasing")
 
