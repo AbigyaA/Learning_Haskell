cc1f974090e769c0a73c4ac50ace932b61253d08
Abigya Ayele <abigya.ayele.12@gmail.com>
2022-07-27 11:27:44 +0300

    modify solution to problem 11

--- 99 Haskell Problems/ques_11_to_20.hs
+++ 99 Haskell Problems/ques_11_to_20.hs
@@ -2,6 +2,15 @@
 data ListItem a = Single a | Multiple Int a
     deriving (Show)
 
+pack :: (Eq a) => [a] -> [[a]]
+pack [] = []
+pack (x:xs) = (x : takeWhile (==x) xs) : pack (dropWhile (==x) xs)
+
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode ls  =  encode' (pack ls)
+          where encode' (x:xs) =  (length x, head x) : encode' xs
+                encode' [] = []
+
 encodeModified :: (Eq a) => [a] -> [ListItem a]
 encodeModified = map encodeHelper . encode
     where
