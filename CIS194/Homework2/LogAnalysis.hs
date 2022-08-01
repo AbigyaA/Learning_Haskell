da2dd370887e478269e998223b03426bdf291d32
Abigya Ayele <abigya.ayele.12@gmail.com>
2022-08-01 11:01:56 +0300

    solve exercise 1 of homework2

--- CIS194/Homework2/LogAnalysis.hs
+++ CIS194/Homework2/LogAnalysis.hs
@@ -1,7 +1,13 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 import Log
+import Data.List
 
 -- Exercise 1
 parseMessage :: String -> LogMessage
-parseMessage str = let 
+parseMessage str = let wordsList = words str in
+            case wordsList of 
+                ("I":ts:msg) -> LogMessage Info (read ts :: Int) (unwords msg)
+                ("W":ts:msg) -> LogMessage Warning (read ts :: Int) (unwords msg)
+                ("E":num:ts:msg) -> LogMessage (Error (read num :: Int)) (read ts :: Int) (unwords msg)
+                _ -> Unknown (unwords wordsList)
