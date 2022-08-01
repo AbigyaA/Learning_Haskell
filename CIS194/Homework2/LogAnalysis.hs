87e7af544be1d1d37f4e67847fe123030616ce3a
Abigya Ayele <abigya.ayele.12@gmail.com>
2022-08-01 15:41:45 +0300

    solve exercises 2,3,4 of homework2

--- CIS194/Homework2/LogAnalysis.hs
+++ CIS194/Homework2/LogAnalysis.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 import Log
-import Data.List
+-- import Data.List
 
 -- Exercise 1
 parseMessage :: String -> LogMessage
@@ -11,3 +11,19 @@ parseMessage str = let wordsList = words str in
                 ("W":ts:msg) -> LogMessage Warning (read ts :: Int) (unwords msg)
                 ("E":num:ts:msg) -> LogMessage (Error (read num :: Int)) (read ts :: Int) (unwords msg)
                 _ -> Unknown (unwords wordsList)
+
+-- Exercise 2
+insert :: LogMessage -> MessageTree -> MessageTree
+insert lgMsg Leaf = Node Leaf lgMsg Leaf
+insert (lgMsg@(LogMessage _ ts1 _)) (msgTree@(Node msgT1 lgMsgTree@(LogMessage _ ts2 _) msgT2)) 
+                | ts1 > ts2 = Node msgTree lgMsg Leaf 
+                | otherwise = Node Leaf lgMsg msgTree
+                    
+-- Exercise 3
+build :: [LogMessage] -> MessageTree 
+build = foldr insert Leaf
+
+-- Exercise 4
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder msgTree@(Node msgT1 lgMsg msgT2) = [lgMsg] ++ (inOrder msgT1) ++ (inOrder msgT2)
